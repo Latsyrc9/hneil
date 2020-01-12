8a2c2c4f5099b23382600cb8efcdd51896ced4ff
latsyrc9 <hneil@HNN-MacBook-Pro.local>
2020-01-12 09:06:57 -0500

    clean up

--- app/src/App.js
+++ app/src/App.js
@@ -10,11 +10,11 @@ class App extends Component {
     return (
       <div id="colorlib-page">
         <div id="container-wrap">
-         	<Sidebar></Sidebar>
+         	{/* <Sidebar></Sidebar> */}
 				  <div id="colorlib-main">
 					  <Introduction></Introduction>
-					  <About></About>
-					  <Timeline></Timeline>
+					  {/* <About></About> */}
+					  {/* <Timeline></Timeline> */}
           </div>
       	</div>
       </div>
