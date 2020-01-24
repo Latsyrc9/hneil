7535ba639ac86c60d7231937da0a62b0635b1a51
latsyrc9 <hneil@HNN-MacBook-Pro.local>
2020-01-24 14:02:30 -0500

    single quotes

--- app/src/components/Sidebar/Sidebar.stories.jsx
+++ app/src/components/Sidebar/Sidebar.stories.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import HomeIcon from "@material-ui/icons/Home";
-import ReceiptIcon from "@material-ui/icons/Receipt";
-import NotificationsIcon from "@material-ui/icons/Notifications";
-import DesktopWindowsIcon from "@material-ui/icons/DesktopWindows";
-import SettingsIcon from "@material-ui/icons/Settings";
+import HomeIcon from '@material-ui/icons/Home';
+import ReceiptIcon from '@material-ui/icons/Receipt';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import DesktopWindowsIcon from '@material-ui/icons/DesktopWindows';
+import SettingsIcon from '@material-ui/icons/Settings';
 import Sidebar from './Sidebar';
 
 function onClick(e, item) {
@@ -11,41 +11,41 @@ function onClick(e, item) {
 }
 
 const items = [
-  { name: "home", label: "Home", Icon: HomeIcon },
+  { name: 'home', label: 'Home', Icon: HomeIcon },
   {
-    name: "billing",
-    label: "Billing",
+    name: 'billing',
+    label: 'Billing',
     Icon: ReceiptIcon,
     items: [
-      { name: "statements", label: "Statements", onClick },
-      { name: "reports", label: "Reports", onClick }
+      { name: 'statements', label: 'Statements', onClick },
+      { name: 'reports', label: 'Reports', onClick }
     ]
   },
-  "divider",
+  'divider',
   {
-    name: "settings",
-    label: "Settings",
+    name: 'settings',
+    label: 'Settings',
     Icon: SettingsIcon,
     items: [
-      { name: "profile", label: "Profile" },
-      { name: "insurance", label: "Insurance", onClick },
-      "divider",
+      { name: 'profile', label: 'Profile' },
+      { name: 'insurance', label: 'Insurance', onClick },
+      'divider',
       {
-        name: "notifications",
-        label: "Notifications",
+        name: 'notifications',
+        label: 'Notifications',
         Icon: NotificationsIcon,
         items: [
-          { name: "email", label: "Email", onClick },
+          { name: 'email', label: 'Email', onClick },
           {
-            name: "desktop",
-            label: "Desktop",
+            name: 'desktop',
+            label: 'Desktop',
             Icon: DesktopWindowsIcon,
             items: [
-              { name: "schedule", label: "Schedule" },
-              { name: "frequency", label: "Frequency" }
+              { name: 'schedule', label: 'Schedule' },
+              { name: 'frequency', label: 'Frequency' }
             ]
           },
-          { name: "sms", label: "SMS" }
+          { name: 'sms', label: 'SMS' }
         ]
       }
     ]
@@ -55,7 +55,7 @@ const items = [
 export const ToStorybook = () => <Sidebar items={items} />;
 
 ToStorybook.story = {
-  name: 'to Storybook',
+  name: 'Menu',
 };
 
 export default {
